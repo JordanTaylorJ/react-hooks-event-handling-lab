19a57b3fbcc2b182cf92e80b77fd4429cee00f44
JordanJoseph <jordant.joseph@outlook.com>
2022-04-20 19:50:39 -0600

    commit tree fbb7443f0e91a17dfd10aa12aa33169d00506080

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,16 @@
 // Code EyesOnMe Component Here
+import React from "react";
+
+function EyesOnMe(){
+    function good() {
+        return console.log("Good!");
+    }
+    function notGood(){
+        return console.log('Hey! Eyes on me!');
+    }
+    return(
+        <button onFocus={good} onBlur={notGood}>Eyes on me</button>
+    )
+}
+
+export default EyesOnMe; 
